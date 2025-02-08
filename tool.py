a3042209275f70d9c7f5d4379fbd799bae56e680
Delta-Water <3627371741@qq.com>
2025-02-08 14:50:29 +0800

    update tool.py

--- tool.py
+++ tool.py
@@ -129,8 +129,8 @@ def create_translations(client, readme_content, main_language):
 def create_links(language, path, main_language=False):
     head = ""
     if main_language:
-        head = f"- [{main_language}](README.md)\n"
-    return head + "\n".join(f"- [{value}]({'{readme_path}/README_{lang}.md'.format(readme_path=path, lang=key)})" for key, value in LANGUAGE_SWITCH_HEADER.items() if key != language and key != main_language)
+        head = f"- [{main_language}](/README.md)\n"
+    return head + "\n".join(f"- [{value}]({'/{readme_path}/README_{lang}.md'.format(readme_path=path, lang=key)})" for key, value in LANGUAGE_SWITCH_HEADER.items() if key != language and key != main_language)
 
 def update_readme_with_links(readme_content, translations, main_language, path):
     readme_with_links = f"{create_links(main_language, path)}\n\n{readme_content}"
